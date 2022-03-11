706c9075f9b8de96d8a566e022cf861bafec35fa
Léonce Aklin <leonceaklin@icloud.com>
2022-03-11 21:41:39 +0100

    Mail changed again

--- resources/views/mail/transfer_order_created.blade.php
+++ resources/views/mail/transfer_order_created.blade.php
@@ -1,8 +1,12 @@
-Hallo
+@php
+\Carbon\Carbon::setlocale(config('app.locale'));
+@endphp
+<p>Hallo</p>
 
-Hier ist der Überweisungsauftrag für alle Exemplare, während des letzten Monats ({{  }}) verkauft wurden oder deren Besitzer:innen ihre Zahlungsdaten nachgeführt haben.
-Der Überweisunsauftrag sollte nach der Erledigung im System als "Erledigt" markiert werden.
+<p>Hier ist der Überweisungsauftrag für alle Exemplare, während des letzten Monats ({{ $transferOrder->created_on->subDays(1)->format("F Y") }}) verkauft wurden oder deren Besitzer:innen ihre Zahlungsdaten nachgeführt haben.
+Der Überweisunsauftrag sollte nach der Erledigung im System als "Erledigt" markiert werden.</p>
 
-Freundliche Grüsse
-Der SchoolHub Bot
-Beep Beep Beep
+<p>Freundliche Grüsse</p>
+<p>Der SchoolHub Bot</p>
+<p>Beep Beep Beep</p>
+<br><br>
