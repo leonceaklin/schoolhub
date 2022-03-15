48107e2a1ee8effb9b306e100c24c83802666738
Léonce Aklin <leonceaklin@icloud.com>
2022-03-15 17:09:39 +0100

    When orders are deleted

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -110,6 +110,7 @@ class WebhookController extends Controller
               $order->delete();
             }
             else{
+              $order->status = "problem";
               $order->calculate();
               $order->save();
             }
